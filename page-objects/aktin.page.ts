46a3bf4f2a6f56eed084f5673dd9dba625a1c281
David Voda <voda.davd@gmail.com>
2026-02-19 11:43:54 +0100

    added 2nd test

--- page-objects/aktin.page.ts
+++ page-objects/aktin.page.ts
@@ -5,11 +5,17 @@ export class AktinPage {
     readonly cartButton: Locator
     readonly cartQuantity: Locator
     readonly notification: Locator
+    readonly shoppingCartLink: Locator
+    readonly removeFromCartButton: Locator
+    readonly emptyShoppingCart: Locator
 
     constructor(page: Page) {
         this.searchbox = page.getByRole('searchbox', { name: 'Hledat' })
         this.cartButton = page.locator('button[value="add_to_cart_button"]').first()
         this.cartQuantity = page.locator('input[name="quantity"]')
         this.notification = page.locator('div[id="notifications-root"]')
+        this.shoppingCartLink = page.locator('a[href="/objednavka"]').first()
+        this.removeFromCartButton = page.locator('button[name="remove"]')
+        this.emptyShoppingCart = page.locator('div[class="shopping-list-empty"]')
     }
 }
--- tests/aktin.spec.ts
+++ tests/aktin.spec.ts
@@ -1,14 +1,16 @@
 import { test, expect } from '@playwright/test';
 import { AktinPage } from '../page-objects/aktin.page';
 
-test('test', async ({ page }) => {
+test('Add specific product to shopping cart', async ({ page }) => {
     const aktinPage = new AktinPage(page);
     const { searchbox, cartButton, notification, cartQuantity } = aktinPage;
+
+    const productName = 'Double Trouble'
 
 
     await page.goto('https://aktin.cz/')
 
-    await searchbox.fill('double trouble')
+    await searchbox.fill(productName)
     await searchbox.press('Enter');
     await page.waitForTimeout(2000)
 
@@ -19,6 +21,32 @@ test('test', async ({ page }) => {
     await cartButton.click()
     await expect(cartQuantity).toHaveValue('1', { timeout: 10000 });
     await expect(notification).toBeVisible()
+
+    //await expect(async () => {
+    //    await cartButton.click();
+    //    await expect(cartQuantity).toHaveValue('1');
+    //}).toPass({ timeout: 10000 });
+
+});
+
+test('Remove specific product from shopping cart', async ({ page }) => {
+    const aktinPage = new AktinPage(page);
+    const { cartButton, cartQuantity, shoppingCartLink, removeFromCartButton, emptyShoppingCart } = aktinPage;
+
+
+    await page.goto('https://aktin.cz/vilgain-double-trouble-protein-bar/arasidove-maslo-malinovy-dzem-55-g-46423')
+
+    await page.waitForTimeout(2000)
+    await cartButton.scrollIntoViewIfNeeded()
+    await cartButton.click()
+    await expect(cartQuantity).toHaveValue('1', { timeout: 10000 });
+
+    await shoppingCartLink.click()
+
+    await removeFromCartButton.click()
+
+    await expect(emptyShoppingCart).toBeVisible()
+    await expect(emptyShoppingCart).toContainText("Košík je prázdný")
 
     //await expect(async () => {
     //    await cartButton.click();
